4bb7406e549d4634a87cdc120f8cb8cdd036c327
Andriy Pelykh <andrew.pelykh@gmail.com>
2023-01-19 15:44:39 +0200

    Initialize Zendesk Messaging SDK

--- Messaging demo/AppDelegate.swift
+++ Messaging demo/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import ZendeskSDKMessaging
+import ZendeskSDK
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,6 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        //Initialize Zendesk Messaging SDK
+        Zendesk.initialize(withChannelKey: "eyJzZXR0aW5nc191cmwiOiJodHRwczovL2Nvbi1sZWFmd29ya3MuemVuZGVzay5jb20vbW9iaWxlX3Nka19hcGkvc2V0dGluZ3MvMDFHTlk0MjVWNUtTSkcxQkJEVEQ5NU5BSlouanNvbiJ9",
+                           messagingFactory: DefaultMessagingFactory()) { result in
+                if case let .failure(error) = result {
+                    print("Messaging did not initialize.\nError: \(error.localizedDescription)")
+                }
+            }
+        
         // Override point for customization after application launch.
         return true
     }
--- Messaging demo/ViewController.swift
+++ Messaging demo/ViewController.swift
@@ -6,13 +6,49 @@
 //
 
 import UIKit
+import ZendeskSDKMessaging
+import ZendeskSDK
+
+// Code is taken from Zendesk official IOS demo app, but replaced parts related to Messaging SDK
+// https://github.com/zendesk/ios_sdk_demo_apps/blob/master/UnifiedSDK/UnifiedSDK/ViewController.swift
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    private var modalBackButton: UIBarButtonItem {
+          UIBarButtonItem(barButtonSystemItem: .close,
+                          target: self,
+                          action: #selector(dismissViewController))
+      }
+    
+    @IBAction func startMessaging(_ sender: Any) {
+        // Create view controller for Messaging SDK
+        guard let viewController = Zendesk.instance?.messaging?.messagingViewController() else { return }
+        presentModally(viewController)
+    }
+    
+    private func pushViewController(_ viewController: UIViewController) {
+            self.navigationController?.pushViewController(viewController, animated: true)
+        }
+
+        private func presentModally(_ viewController: UIViewController,
+                                    presenationStyle: UIModalPresentationStyle = .automatic) {
+            viewController.navigationItem.leftBarButtonItem = modalBackButton
+            
+            let navigationController = UINavigationController(rootViewController: viewController)
+            navigationController.modalPresentationStyle = presenationStyle
+
+            present(navigationController, animated: true)
+            
+            self.navigationController?.show(viewController, sender: self)
+        }
+
+        /// Dismiss modal `viewController` action
+        @objc private func dismissViewController() {
+            navigationController?.dismiss(animated: true, completion: nil)
+        }
 
 
 }
