0fd7a7b655591235282fe11c70961e4bc8adf2ee
Andriy Pelykh <andrew.pelykh@gmail.com>
2023-01-19 14:43:29 +0200

    Initial Commit

---
+++ Messaging demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messaging demo
+//
+//  Created by Andriy Pelykh on 19.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
